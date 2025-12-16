2b9901a2f9d2a936501f884200b3965904daa3ee
peiyuan.wang <peiyuan.wang@lotusflare.com>
2025-12-16 17:13:36 +0800

    add: brew tap

---
+++ Formula/unify.rb
@@ -0,0 +1,39 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://docs.brew.sh/rubydoc/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Unify < Formula
+  desc "u guess😄"
+  homepage "https://github.com/peiyuanwang0/homebrew-unify"
+  url "https://github.com/peiyuanwang0/homebrew-unify/releases/download/release-darwin-arm64/unify-darwin-arm64.tgz"
+  sha256 "9fdc241cd341d48324553499935a13873e63afd772b6d24644a8315fbd965635"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    # Remove unrecognized options if they cause configure to fail
+    # https://docs.brew.sh/rubydoc/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "unify-linux-arm64" => "unify"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test unify`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
